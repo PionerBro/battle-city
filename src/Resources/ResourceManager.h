e4935b9d8bbe41f0b26776c6801100deb29c33bc
PionerBro <hobbi44pioner@mail.ru>
2021-03-03 08:52:44 +0300

    Added Texture2D class

--- src/Resources/ResourceManager.h
+++ src/Resources/ResourceManager.h
@@ -6,6 +6,7 @@
 
 namespace Renderer {
 	class ShaderProgram;
+	class Texture2D;
 }
 
 
@@ -20,12 +21,17 @@ public:
 
 	std::shared_ptr<Renderer::ShaderProgram> loadShaders(const std::string& shaderName, const std::string& vertexPath, const std::string& FragmentPath);
 	std::shared_ptr<Renderer::ShaderProgram> getShaderProgram(const std::string& shaderName);
-
+	std::shared_ptr<Renderer::Texture2D> loadTexture(const std::string& textureName, const std::string& texturePath);
+	std::shared_ptr<Renderer::Texture2D> getShaderTexture(const std::string& textureName);
 
 private:
 	std::string getFileString(const std::string& relativeFilePath) const;
 
 	typedef std::map<const std::string, std::shared_ptr<Renderer::ShaderProgram>> ShaderProgramsMap;
 	ShaderProgramsMap m_shaderProgram;
+	
+	typedef std::map<const std::string, std::shared_ptr<Renderer::Texture2D>> TexturesMap;
+	TexturesMap m_textures;
+
 	std::string m_path;
 };
